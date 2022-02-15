669f22b3da4bbefa0ec03de6123289ab7104777d
hossam-khalaf <hossamhassan303089@gmail.com>
2022-02-16 03:00:50 +0400

    font

---
+++ components/Fonts.js
@@ -0,0 +1,9 @@
+import { Global } from '@emotion/react'
+
+const Fonts = () => (
+	<Global
+		styles={`@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&family=Outfit:wght@400;500;900&display=swap');`}
+	/>
+)
+
+export default Fonts
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Main from '../components/layouts/Main'
+import Fonts from '../components/Fonts'
 import theme from '../libs/theme'
 
 function MyApp({ Component, pageProps, router }) {
 	return (
 		<ChakraProvider theme={theme}>
+			<Fonts />
 			<Main router={router}>
 				<Component {...pageProps} key={router.route} />
 			</Main>
