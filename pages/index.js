2eaf9468cc9c4edd98db2800e7ff895a0012d3cd
hossam-khalaf <hossamhassan303089@gmail.com>
2022-02-16 03:42:48 +0400

    profile image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Box, Container, Heading } from '@chakra-ui/react'
+import {
+	Box,
+	Container,
+	Heading,
+	Image,
+	useColorModeValue,
+} from '@chakra-ui/react'
 
 const Page = () => {
 	return (
 		<Container>
-			<Box borderRadius='lg' bg='teal' p={4} mb={6} align='center'>
+			<Box
+				borderRadius='lg'
+				bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+				p={4}
+				mt={2}
+				mb={6}
+				align='center'>
 				Hello , I&apos;m a Front-End Developer Based in Dubai, UAE
 			</Box>
 
@@ -13,6 +25,23 @@ const Page = () => {
 						Hossam Khalaf
 					</Heading>
 					<p>Software Developer (Front-End / ReactJs /NextJs) </p>
+				</Box>
+				<Box
+					flexShrink={0}
+					mt={{ base: 4, md: 0 }}
+					ml={{ md: 6 }}
+					align='center'>
+					<Image
+						borderColor='whiteAlpha.800'
+						borderWidth={4}
+						borderStyle='solid'
+						objectFit='cover'
+						maxWidth='100px'
+						display='inline-block'
+						borderRadius='full'
+						src='/images/hosshhkh.jpg'
+						alt='Profile Image'
+					/>
 				</Box>
 			</Box>
 		</Container>
