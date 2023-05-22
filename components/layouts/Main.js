1585297ed667e53e48d6a4d4456d5732081f18da
Hossam khalaf <57095049+hossam-khalaf@users.noreply.github.com>
2023-05-22 11:45:34 +0700

    Update Main.js

--- components/layouts/Main.js
+++ components/layouts/Main.js
@@ -8,6 +8,7 @@ const Main = ({ children, router }) => {
 			<Head>
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 				<title>Hossam Khalaf - Homepage</title>
+<meta name="description" content="Discover the exceptional work of Hossam Khalaf, a proficient Software Developer specializing in Front-End and WordPress. Dive into his skillful artistry as a WordPress designer and developer. Explore now!">
 			</Head>
 			<Navbar path={router.asPath} />
 			<Container maxW='container.md' pt={14}>
